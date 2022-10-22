8d4cdf69baeadb7ef119168abf5914e97e3880fb
Filipe Augusto <filipeaugusto@alunos.utfpr.edu.br>
2022-10-22 19:37:53 -0300

    Aula 09 - Manipulação de texto

---
+++ Refazendo_Exercicios/aula_09.py
@@ -0,0 +1,16 @@
+''' Manipulação de Texto em Python
+    Funções importante para manipulação de texto:
+        # Analise:
+            - len(frase): retorna o tamanho de frase.
+            
+            - frase.count(char/string): retorna quantas vezes ele encontrou o char ou string
+            colocado como parametro. Também pode ser da forma (char/string, inicio, fim),
+            para procurar o char ou string em certos limites da frase.
+            
+            - frase.find(char/string): procura dentro de frase o char ou string colocado como
+            parametro, e retorna a posição do primeiro char dentro de frase, caso não encontre
+            ele retorna -1.
+            
+            -  
+
+'''
